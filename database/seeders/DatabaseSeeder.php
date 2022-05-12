e3acfe3670ce53614a60a78f0b92c9a49541f763
vu hiep <vuxhiep97@gmail.com>
2022-05-12 18:19:26 +0700

    second commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use app\User;
+use app\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +17,42 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // $this->call(UsersTableSeeder::class);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::insert([
+            [
+                'name'            => 'Mr. Admin',
+                'email'           => 'admin@admin.com',
+                'password'        => bcrypt('123456'),
+                'role_id'         => 1,
+                'status'          => 1,
+                'remember_token'  => Str::random(10),
+                'created_at'      => date("Y-m-d H:i:s")
+            ],
+            [
+                'name'            => 'Mr. Editor',
+                'email'           => 'editor@editor.com',
+                'password'        => bcrypt('123456'),
+                'role_id'         => 2,
+                'status'          => 1,
+                'remember_token'  => Str::random(10),
+                'created_at'      => date("Y-m-d H:i:s")
+            ],
+            [
+                'name'            => 'Mr. User',
+                'email'           => 'user@user.com',
+                'password'        => bcrypt('123456'),
+                'role_id'         => 3,
+                'status'          => 1,
+                'remember_token'  => Str::random(10),
+                'created_at'      => date("Y-m-d H:i:s")
+            ]
+        ]);
+
+        Role::insert([
+            ['name' => 'Admin', 'slug' => 'admin'],
+            ['name' => 'Editor', 'slug' => 'editor'],
+            ['name' => 'User',  'slug' => 'user']
+        ]);
     }
 }
